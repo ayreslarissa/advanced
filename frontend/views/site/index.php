c99c0cada63ad7140d9ba3e39833e6b3a0b985a7
Larissa Ayres <larimayres@gmail.com>
2016-12-27 22:58:41 -0400

    enviando projeto

---
+++ frontend/views/site/index.php
@@ -0,0 +1,24 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'SCM Portal';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Wecolme to SCM Portal!</h1>
+
+        <p class="lead">Here you can find all tools used by SiDIA SCM Team.</p>
+
+     
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+          
+        </div>
+
+    </div>
+</div>
